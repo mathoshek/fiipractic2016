9a1386e3cd6f6bd0b8211ef49ccb3873f68b78b3
Albert Chmilevski <a.chmilevski@levi9.com>
2016-03-15 10:57:37 +0200

    added logging and security.

--- rest/src/main/java/com/fiipractic/agenda/rest/controllers/UsersController.java
+++ rest/src/main/java/com/fiipractic/agenda/rest/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.fiipractic.agenda.rest.controllers;
 import com.fiipractic.agenda.rest.models.User;
 import com.fiipractic.agenda.rest.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +20,28 @@ public class UsersController {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userService.getAllUsers();
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{username}")
-    public User getUser(@PathVariable String username){
+    @PreAuthorize("authentication.name == #username")
+    @RequestMapping(method = RequestMethod.GET, path = "/{username:.+}")
+    public User getUser(@PathVariable String username) {
         return userService.getUserByUsername(username);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
-    @RequestMapping(method=RequestMethod.PUT, path = "/{username}")
-    public User updateUser(@PathVariable String username, @RequestBody User user){
+    @RequestMapping(method = RequestMethod.PUT, path = "/{username:.+}")
+    public User updateUser(@PathVariable String username, @RequestBody User user) {
         return userService.updateUser(username, user);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/{username}")
-    public void deleteUser(@PathVariable String username){
+    @RequestMapping(method = RequestMethod.DELETE, path = "/{username:.+}")
+    public void deleteUser(@PathVariable String username) {
         userService.deleteUser(username);
     }
 }
